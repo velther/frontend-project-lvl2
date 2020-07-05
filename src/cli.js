b3919202f8f466e4a186be06f6f1101b4a94dfa7
velther <velthers@gmail.com>
2020-07-05 20:53:09 +0300

    Adds naive json comparator implementation

---
+++ src/cli.js
@@ -0,0 +1,23 @@
+import path from 'path';
+import {readFileSync} from 'fs';
+
+import commander from 'commander';
+
+import runAction from './run-action.js';
+
+const {program} = commander;
+
+const {version} = JSON.parse(readFileSync(path.join(process.cwd(), './package.json')));
+
+const cli = () => {
+  program
+    .version(version)
+    .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format [type]', 'output format')
+    .arguments('<filepath1> <filepath2>')
+    .action(runAction);
+
+  program.parse(process.argv);
+}
+
+export default cli;
---
+++ src/index.js
@@ -0,0 +1,23 @@
+const gendiff = (original, compare) => {
+  const diff = [];
+  const diffKeys = new Set(Object.keys(original).concat(Object.keys(compare)));
+
+  for (const key of diffKeys) {
+    if (key in original) {
+      if (original[key] === compare[key]) {
+        diff.push(`    ${key}: ${original[key]}`);
+      } else {
+        diff.push(`  - ${key}: ${original[key]}`);
+
+        if (key in compare) {
+          diff.push(`  + ${key}: ${compare[key]}`);
+        }
+      }
+    } else {
+      diff.push(`  + ${key}: ${compare[key]}`)
+    }
+  }
+  return `{\n${diff.join('\n')}\n}`;
+};
+
+export default gendiff;
